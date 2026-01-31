17fc6fbb3a95d06b7384537099a803499fd6305f
t7 <tineescosio001@gmail.com>
2026-01-31 16:13:48 +0800

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,173 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        BtnCompute = New Button()
+        TxtStud = New TextBox()
+        TxtMidterm = New TextBox()
+        LblStud = New Label()
+        LblMidterm = New Label()
+        TxtID = New TextBox()
+        LblID = New Label()
+        PictureBox2 = New PictureBox()
+        PictureBox3 = New PictureBox()
+        LblD1 = New Label()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' BtnCompute
+        ' 
+        BtnCompute.Font = New Font("Pixelify Sans", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        BtnCompute.Location = New Point(331, 395)
+        BtnCompute.Name = "BtnCompute"
+        BtnCompute.Size = New Size(143, 29)
+        BtnCompute.TabIndex = 0
+        BtnCompute.Text = "Compute"
+        BtnCompute.UseVisualStyleBackColor = True
+        ' 
+        ' TxtStud
+        ' 
+        TxtStud.Location = New Point(124, 184)
+        TxtStud.Name = "TxtStud"
+        TxtStud.Size = New Size(198, 27)
+        TxtStud.TabIndex = 1
+        TxtStud.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' TxtMidterm
+        ' 
+        TxtMidterm.Location = New Point(504, 182)
+        TxtMidterm.Name = "TxtMidterm"
+        TxtMidterm.Size = New Size(155, 27)
+        TxtMidterm.TabIndex = 2
+        TxtMidterm.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' LblStud
+        ' 
+        LblStud.AutoSize = True
+        LblStud.Font = New Font("Pixelify Sans", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        LblStud.ForeColor = Color.LightPink
+        LblStud.Location = New Point(188, 161)
+        LblStud.Name = "LblStud"
+        LblStud.Size = New Size(71, 18)
+        LblStud.TabIndex = 3
+        LblStud.Text = "Student"
+        ' 
+        ' LblMidterm
+        ' 
+        LblMidterm.AutoSize = True
+        LblMidterm.Font = New Font("Pixelify Sans", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        LblMidterm.ForeColor = Color.LightPink
+        LblMidterm.Location = New Point(524, 161)
+        LblMidterm.Name = "LblMidterm"
+        LblMidterm.Size = New Size(120, 18)
+        LblMidterm.TabIndex = 4
+        LblMidterm.Text = "Midterm Grade"
+        ' 
+        ' TxtID
+        ' 
+        TxtID.Location = New Point(349, 184)
+        TxtID.Name = "TxtID"
+        TxtID.Size = New Size(125, 27)
+        TxtID.TabIndex = 5
+        TxtID.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' LblID
+        ' 
+        LblID.AutoSize = True
+        LblID.Font = New Font("Pixelify Sans", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        LblID.ForeColor = Color.LightPink
+        LblID.Location = New Point(398, 161)
+        LblID.Name = "LblID"
+        LblID.Size = New Size(23, 18)
+        LblID.TabIndex = 6
+        LblID.Text = "ID"
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), Image)
+        PictureBox2.Location = New Point(539, 239)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(287, 255)
+        PictureBox2.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox2.TabIndex = 8
+        PictureBox2.TabStop = False
+        ' 
+        ' PictureBox3
+        ' 
+        PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), Image)
+        PictureBox3.Location = New Point(-28, 239)
+        PictureBox3.Name = "PictureBox3"
+        PictureBox3.Size = New Size(287, 255)
+        PictureBox3.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox3.TabIndex = 9
+        PictureBox3.TabStop = False
+        ' 
+        ' LblD1
+        ' 
+        LblD1.AutoSize = True
+        LblD1.Font = New Font("00 Starmap Truetype", 24F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        LblD1.Location = New Point(107, 74)
+        LblD1.Name = "LblD1"
+        LblD1.Size = New Size(585, 35)
+        LblD1.TabIndex = 10
+        LblD1.Text = "MIDTERM GRADE CALCULATOR"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.LavenderBlush
+        ClientSize = New Size(800, 450)
+        Controls.Add(LblD1)
+        Controls.Add(PictureBox3)
+        Controls.Add(PictureBox2)
+        Controls.Add(LblID)
+        Controls.Add(TxtID)
+        Controls.Add(LblMidterm)
+        Controls.Add(LblStud)
+        Controls.Add(TxtMidterm)
+        Controls.Add(TxtStud)
+        Controls.Add(BtnCompute)
+        ForeColor = Color.LightPink
+        Name = "Form1"
+        Text = "Midterm Grade Calculator"
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents BtnCompute As Button
+    Friend WithEvents TxtStud As TextBox
+    Friend WithEvents TxtMidterm As TextBox
+    Friend WithEvents LblStud As Label
+    Friend WithEvents LblMidterm As Label
+    Friend WithEvents TxtID As TextBox
+    Friend WithEvents LblID As Label
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents LblD1 As Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,57 @@
+﻿Public Class Form1
+
+    Function GetInput(prompt As String, title As String) As String
+        Return InputBox(prompt, title)
+    End Function
+
+    Shared Function GetScore(prompt As String) As Double
+        Dim handleInput As String
+        Dim score As Double
+        Dim isit60 As Boolean = False
+
+        Do
+            handleInput = InputBox(prompt, "Score Entry (60-100)")
+
+            If Double.TryParse(handleInput, score) AndAlso score >= 60 AndAlso score <= 100 Then
+                isit60 = True
+            Else
+                MsgBox("Invalid input! Please enter a score between 60 and 100.", MsgBoxStyle.Exclamation, "Incorrect Input")
+            End If
+        Loop While isit60 = False
+
+        Return score
+    End Function
+
+    Shared Function ComputeCS(mq1 As Double, mq2 As Double, mq3 As Double, ml1 As Double, ml2 As Double, ml3 As Double) As Double
+        Return (2 / 3 * ((mq1 + mq2 + mq3) / 3)) + (1 / 3 * ((ml1 + ml2 + ml3) / 3))
+    End Function
+
+    Shared Function ComputeMG(cs As Double, meGrade As Double) As Double
+        Return (2 / 3 * cs) + (1 / 3 * meGrade)
+    End Function
+
+    Private Sub BtnCompute_Click(sender As Object, e As EventArgs) Handles BtnCompute.Click
+        Dim studname As String = GetInput("Enter Student Name:", "Student Name")
+        Dim ID As String = GetInput("Enter ID Number:", "Your ID")
+
+        Dim mq1 As Double = GetScore("Enter Midterm Quiz 1:")
+        Dim mq2 As Double = GetScore("Enter Midterm Quiz 2:")
+        Dim mq3 As Double = GetScore("Enter Midterm Quiz 3:")
+        Dim ml1 As Double = GetScore("Enter Lab 1:")
+        Dim ml2 As Double = GetScore("Enter Lab 2:")
+        Dim ml3 As Double = GetScore("Enter Lab 3:")
+        Dim exam As Double = GetScore("Enter Midterm Exam:")
+
+        Dim classStanding As Double = ComputeCS(mq1, mq2, mq3, ml1, ml2, ml3)
+        Dim midtermGrade As Double = ComputeMG(classStanding, exam)
+
+
+        TxtStud.Text = studname
+        TxtMidterm.Text = Format(midtermGrade / 100, "Percent")
+        TxtID.Text = ID
+    End Sub
+
+    Private Sub LblMidterm_Click(sender As Object, e As EventArgs) Handles LblMidterm.Click
+
+    End Sub
+End Class
